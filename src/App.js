0bb0d3e3e89ccefe42b4345a18fee93d9d5912be
Alyssa Krouson <a2krouson@gmail.com>
2024-09-25 00:56:43 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MyPage from './MyPage'; // Import your newly created page
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+
+        <div>
+
+            <MyPage />
+
+            {/* Other parts of your app */}
+
+        </div>
+
+    );
+
 }
+
+
 
 export default App;
---
+++ src/MyPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+function MyPage() {
+
+    return (
+
+        <div>
+
+            <h1>Welcome to My Page!</h1>
+
+            {/* Add your page content here */}
+
+        </div>
+
+    );
+
+}
+
+
+
+export default MyPage;
