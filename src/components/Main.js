41e87659148f2ad59c5a8337aa7d2257c99bb0ba
Alyssa Krouson <a2krouson@gmail.com>
2024-09-25 14:31:51 -0400

    description

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,9 +6,11 @@ export default function Main() {
         <main className='bottom'>
             <div className='par'>
                 <h2>About</h2>
-                    <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+                    <p>I am a graduated student from the University of Rhode Island where I studied computer science and Japanese. 
+                        I am interested in web development and furthering my knowledge in design and functionality. I currently work well with HTML, CSS, JavaScript, and I am learning new frameworks like React.
+                        I try to continue practicing my skills, and am always looking for new things to learn.</p>
                 <h2>Intrests</h2>
-                    <p>Reader. Music scholar. Internet fanatic. Travel lover. Pop culture ninja. Coffee fanatic.</p>
+                    <p>Reader. Artis. Music scholar. Internet fanatic. Travel lover. Pop culture ninja. Coffee fanatic.</p>
             </div>
         </main>
     )
