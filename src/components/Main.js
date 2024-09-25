4aef87d04e7d1828a2caee845c3dbfa02eef43eb
Alyssa Krouson <a2krouson@gmail.com>
2024-09-25 14:43:33 -0400

    spelling

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ export default function Main() {
                     <p>I am a graduated student from the University of Rhode Island where I studied computer science and Japanese. 
                         I am interested in web development and furthering my knowledge in design and functionality. I currently work well with HTML, CSS, JavaScript, and I am learning new frameworks like React.
                         I try to continue practicing my skills, and am always looking for new things to learn.</p>
-                <h2>Intrests</h2>
+                <h2>Interests</h2>
                     <p>Reader. Artis. Music scholar. Internet fanatic. Travel lover. Pop culture ninja. Coffee fanatic.</p>
             </div>
         </main>
